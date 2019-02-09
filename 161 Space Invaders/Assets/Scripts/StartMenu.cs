62261330b799992e8eda3ed4a419c99e8c140c73
Gabe Eck <eck.gabe@gmail.com>
2019-02-08 22:07:15 -0800

    GameOver overlay
    has a play again button and quit button

--- 161 Space Invaders/Assets/Scripts/StartMenu.cs
+++ 161 Space Invaders/Assets/Scripts/StartMenu.cs
@@ -7,7 +7,7 @@ public class StartMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene("GabeScene");
     }
 
     public void ExitGame()
