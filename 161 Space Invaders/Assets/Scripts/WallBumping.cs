b2520dfa7c9121d3ef38adf7069549f22e222a95
Josef Bertolini <bertolij@uci.edu>
2019-02-09 18:54:34 -0800

    Enemies bumping into walls

---
+++ 161 Space Invaders/Assets/Scripts/WallBumping.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WallBumping : MonoBehaviour
+{
+    private bool wasBumped = false;
+
+    public UnityEvent OnWallBumpEvent = new UnityEvent();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!wasBumped)
+        {
+            OnWallBumpEvent.Invoke();
+            wasBumped = true;
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        wasBumped = false;
+    }
+}
