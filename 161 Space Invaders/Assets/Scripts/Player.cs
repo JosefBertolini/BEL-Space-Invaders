c3dec86204a28a6a7e009128e13f5bd174d4664e
gabeeck <eck.gabe@gmail.com>
2019-02-09 17:20:19 -0800

    Added player with movement

---
+++ 161 Space Invaders/Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] public int lives = 3;
+    [SerializeField] public bool canFire = true;
+    [SerializeField] protected float speed = 5;
+    private Rigidbody2D m_rigidbody;
+    private Collider2D m_collider;
+    public int score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = this.GetComponent<Rigidbody2D>();
+        m_collider = this.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+
+    void Move()
+    {
+        float movementModifier = Input.GetAxisRaw("Horizontal");
+        Vector2 currentVelocity = m_rigidbody.velocity;
+        m_rigidbody.velocity = new Vector2(movementModifier * speed, currentVelocity.y);
+    }
+
+    void Shoot()
+    {
+
+    }
+}
