6552b02971f3a415d70367e9b72375fd08a88d8c
Parker Levin <pedker@gmail.com>
2019-02-10 01:07:42 -0800

    Added random enemy shooting

--- 161 Space Invaders/Assets/Scripts/GameManager.cs
+++ 161 Space Invaders/Assets/Scripts/GameManager.cs
@@ -12,24 +12,54 @@ public class GameManager : MonoBehaviour
     public List<List<GameObject>> alienGrid = new List<List<GameObject>>();
 
     [SerializeField] protected float maxTimeToShoot;
-    private float timeElapsed = 0;
-    private List<int> checkedColumns = new List<int>();
+    private float timeElapsed;
+    private float randomTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeElapsed = 0.0f;
+        randomTime = Random.Range(0.0f, maxTimeToShoot);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > randomTime)
+        {
+            FindShooter();
+            randomTime = Random.Range(0.0f, maxTimeToShoot);
+            timeElapsed = 0.0f;
+        }
     }
 
     public void PlayerDead()
     {
         Time.timeScale = 0;
         gameOverOverlay.SetActive(true);
+    }
+
+    void FindShooter()
+    {
+        int[] temp = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        List<int> columnsToCheck = new List<int>(temp);
+
+        for (int x = 0; x < 11; x++)
+        {
+            int index = Random.Range(0, 11-x);
+
+            for (int y = 0; y < 5; y++)
+            {
+                if (alienGrid[index][y].CompareTag("Enemy"))
+                {
+                    Enemy foundEnemy = alienGrid[index][y].GetComponent<Enemy>();
+                    foundEnemy.Shoot();
+                    return;
+                }
+            }
+
+            columnsToCheck.RemoveAt(index);
+        }
     }
 }
